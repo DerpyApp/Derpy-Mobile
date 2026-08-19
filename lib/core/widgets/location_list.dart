4b33b6bbe7d758066317eef7383eac8df4597935
Abdelrhman Muharram <abdomu794@gmail.com>
2026-08-20 02:04:35 +0300

    feat: implement dynamic city selection and update LocationItem UI

--- lib/core/widgets/location_list.dart
+++ lib/core/widgets/location_list.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../../features/home/data/model/city_model.dart';
-import '../../features/home/presentation/cubit/location_cubit.dart';
 import '../theme/app_colors.dart';
 import '../theme/app_text_styles.dart';
 
